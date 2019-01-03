36a834deb7e69381f718783dde2762d06babfd75
Balázs Orbán <info@balazsorban.com>
2019-01-03 10:57:08 +0100

    :pencil2: minor changes

--- functions/toFirestore.js
+++ functions/toFirestore.js
@@ -1,22 +1,24 @@
 const admin = require("firebase-admin")
 const serviceAccount = require("./service-account-credentials.json")
-const feedbacks = require("./data.json")
+const data = require("./data.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  feedbacksbaseURL: "https://bibic-vendeghazak-api.firebaseio.com"
+  databaseURL: "https://bibic-vendeghazak-api.firebaseio.com"
 })
 admin.firestore().settings({timestampsInSnapshots: true})
 
-feedbacks && Object.keys(feedbacks).forEach(key => {
-  const nestedContent = feedbacks[key]
+Object.keys(data)
+  .forEach(key => {
+    const nestedContent = data[key]
 
-  if (typeof nestedContent === "object") {
-    Object.keys(nestedContent).forEach(docTitle => {
-      admin.firestore()
-        .collection(key)
-        .doc(docTitle)
-        .set(nestedContent[docTitle])
-    })
-  }
-})
+    if (typeof nestedContent === "object") {
+      Object.keys(nestedContent)
+        .forEach(docTitle => {
+          admin.firestore()
+            .collection(key)
+            .doc(docTitle)
+            .set(nestedContent[docTitle])
+        })
+    }
+  })
