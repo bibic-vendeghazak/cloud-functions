2c8da4723f3fdd6b3f66492a6d4d9a9c6d5bbdc5
Balázs Orbán <info@balazsorban.com>
2019-01-08 08:59:28 +0100

    update functions to support roomId arrays
    #5

--- functions/src/lib/reservations/overlaps.js
+++ functions/src/lib/reservations/overlaps.js
@@ -9,24 +9,25 @@ export const getOverlaps = ({query: {roomId}}, res) => {
   // NOTE: Change origin to bibicvendeghazak.hu
   res.header("Access-Control-Allow-Origin", "*")
   const overlaps = []
-  return Promise.all([
-    ROOMS_DB.child(`${roomId - 1}/unavailable`).once("value"),
-    RESERVATIONS_FS
-      .where("roomId", "==", parseInt(roomId, 10))
-      .where("to", ">=", subDays(TODAY, 1))
-      .get()
-  ]).then(([unavailable, reservations]) => {
-    if (unavailable.exists()) {
-      overlaps.push({start: TODAY, end: new Date(unavailable.val())})
-    }
-    if (!reservations.empty) {
-      reservations.forEach(reservation => {
-        const {from, to} = reservation.data()
-        overlaps.push({start: from.toDate(), end: to.toDate()})
-      })
-    }
-    return res.send(JSON.stringify(overlaps))
-  })
+  
+  const unavailable = await ROOMS_DB.child(`${roomId - 1}/unavailable`).once("value")
+  
+  const reservations = await RESERVATIONS_FS
+    .where("roomId", "array-contains", parseInt(roomId, 10))
+    .where("to", ">=", subDays(TODAY, 1))
+    .get()
+
+  if (unavailable.exists()) {
+    overlaps.push({start: TODAY, end: new Date(unavailable.val())})
+  }
+  if (!reservations.empty) {
+    reservations.forEach(reservation => {
+      const {from, to} = reservation.data()
+      overlaps.push({start: from.toDate(), end: to.toDate()})
+    })
+  }
+
+  return res.send(JSON.stringify(overlaps))
 
 }
 
--- functions/src/lib/reservations/utils.js
+++ functions/src/lib/reservations/utils.js
@@ -1,20 +1,19 @@
 import moment from "../moment"
-import { database } from "../firebase"
+import { RESERVATION_DATES_DB } from "../firebase"
 
-const reservationDatesRef = database.ref("reservationDates")
 
 
 /**
  * use when an update (create/change/delete) occurs on a reservation.
  * @param {Timestamp} from first date to check
  * @param {Timestamp} to last date to check
- * @param {number} roomId which room should be checked
+ * @param {array} roomIds which rooms should be checked
  * @param {string} reservationId which reservation changed
  * @param {boolean} [shouldDelete=false] is it a delete operation?
  * @param {boolean} [compare=] has arrival, departure or roomId changed?
  * @returns {Promise}
  */
-export const updateReservationDates = (from, to, roomId, reservationId, shouldDelete=false) => {
+export function updateReservationDates(from, to, roomIds, reservationId, shouldDelete=false) {
   // convert from Firebase Timestamp
   from = moment(from.toDate())
   to = moment(to.toDate())
@@ -23,18 +22,27 @@ export const updateReservationDates = (from, to, roomId, reservationId, shouldDe
   /* iterate over all the dates from arrival to departure
    * REVIEW: an extra day must be added to "to" date for it to work
    */
-  return Promise.all(Array.from(moment.range(from, to.clone().add(1, "day")).by("day")).map(day => {
-    let newReservation = null
-    if (!shouldDelete) {
-      newReservation = {
-        from: day.isSame(from, "day"),
-        to: day.isSame(to, "day")
-      }
-    }
-    return reservationDatesRef
-      .child(`${day.format("YYYY/MM/DD")}/r${roomId}/${reservationId}`)
-      .set(newReservation)
-  }))
+  const reservations = Array.from(moment.range(from, to.clone().add(1, "day")).by("day"))
+    .map(day => ({
+        reservation: !shouldDelete ? {
+          from: day.isSame(from, "day"),
+          to: day.isSame(to, "day")
+        } : null,
+        day
+      })
+    )
+
+  
+  return Promise.all(reservations
+    .map(({reservation, day}) =>
+      Promise.all(roomIds.map(roomId =>
+        RESERVATION_DATES_DB
+          .child(`${day.format("YYYY/MM/DD")}/r${roomId}/${reservationId}`)
+          .set(reservation)
+        )
+      )
+    )
+  )
 }
 
 /**
